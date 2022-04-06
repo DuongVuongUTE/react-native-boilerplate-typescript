12cf86e1f7a897f4e5abac3dd5889a2d5ed3fd96
Le Hau <lehau@C2Cs-MacBook-Pro.local>
2022-04-06 14:31:28 +0700

    Update i18n

---
+++ template/src/i18n/index.ts
@@ -0,0 +1,19 @@
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+import * as resources from './resources';
+
+i18n.use(initReactI18next).init({
+  compatibilityJSON: 'v3',
+  resources: {
+    ...Object.entries(resources).reduce((acc, [key, value]) => {
+      return {
+        ...acc,
+        [key]: value,
+      };
+    }, {}),
+  },
+  lng: 'en',
+  fallbackLng: 'en',
+});
+
+export default i18n;
